d006f71b68c562f7e4f564ccc0153565cc04246d
Aoyama Momoka <aoyama_momoka@giginc.co.jp>
2024-10-12 19:36:24 +0900

    reason section

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,4 +1,10 @@
 module.exports = {
   extends: ['stylelint-config-standard-scss', 'stylelint-config-recess-order'],
   plugins: ['stylelint-order'],
+  rules: {
+    'selector-id-pattern': null, // idでkebab-case以外も許容
+    'selector-class-pattern': null, // classでkebab-case以外も許容
+    'keyframes-name-pattern': null, // keyframesでkebab-case以外も許容
+    'scss/at-mixin-pattern': null, // mixinでkebab-case以外も許容
+  },
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 // import Image from 'next/image';
 // import styles from './page.module.scss';
 import Kv from './section/kv';
+import ReasonSection from './section/reason';
 
 export default function Home() {
   return (
     <main>
       <Kv />
+      <ReasonSection />
     </main>
   );
 }
